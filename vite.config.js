de7bb8369e83cb1fb91b0a993b045c5f27068ed8
Denis <denisbormusov@gmail.com>
2026-06-23 22:23:25 +0500

    feat(svgSprite): add vite-plugin-svg-icons-ng
    I added vite-plugin-svg-icons-ng so that it
    collects one svg and inserts it
    into the html file.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import path from 'path';
 import eslintPlugin from 'vite-plugin-eslint';
 import injectHTML from 'vite-plugin-html-inject';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons-ng'
 
 export default defineConfig({
-  plugins: [ injectHTML(), // Включаем плагин
+  plugins: [ 
+    injectHTML(), // Включаем плагин
     eslintPlugin({
       // Опции: можно указать, включать ли предупреждения, кэш и т.д.
       cache: false,
       include: ['**/*.js'],
       exclude: ['node_modules/**', 'dist/**'],
+    }),
+    createSvgIconsPlugin({
+      iconDirs: ['src/assets/icons'],
+      failOnError: true,
+      // Оптимизация SVG с помощью SVGO (настройка под ваш стиль)
+      svgoOptions: {
+        multipass: true, // многопроходная оптимизация
+      }
     }),
   ],
 });
