cae84c3589af7b0ed9ff7e549ab92ebf2e4b9139
Denis <denisbormusov@gmail.com>
2026-06-23 10:35:52 +0500

    build(vite): Add injectHTML and settings to it
    Add the injectHTML import and set the settings in
    the vite.config.js file in the defineConfig
    function

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import eslintPlugin from 'vite-plugin-eslint';
+import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
-  plugins: [
+  plugins: [ injectHTML(), // Включаем плагин
     eslintPlugin({
       // Опции: можно указать, включать ли предупреждения, кэш и т.д.
       cache: false,
