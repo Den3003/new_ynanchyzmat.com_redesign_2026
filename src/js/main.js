6ebe271ce7f7fae68f35587ae58e24d6832ddac8
Denis <denisbormusov@gmail.com>
2026-06-23 15:36:44 +0500

    feat(timeZone): add module timeZone.js
    I added the timeZone.js module, and now when a
    user visits the website, they are shown the time
    in Turkmenistan, regardless of their country.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 import '../styles/main.scss';
+import { tick } from './modules/timeZone';
 
-
+tick();
---
+++ src/js/modules/timeZone.js
@@ -0,0 +1,18 @@
+// 1. Инициализируем форматировщик ОДИН раз (Best Practice для перформанса)
+const ashgabatFormatter = new Intl.DateTimeFormat('ru-RU', {
+  timeZone: 'Asia/Ashgabat',
+  hour: '2-digit',
+  minute: '2-digit',
+  // hour12: false // Использовать 24-часовой формат. Поменяйте на true, если нужен AM/PM
+});
+
+const clockElement = document.querySelector('.header__time');
+
+// 2. Функция, которая берет системное время юзера и «сдвигает» его отображение под Ашхабад
+export function tick(){
+  const now = new Date();
+  clockElement.textContent = ashgabatFormatter.format(now);
+}
+
+// 4. Запускаем интервал каждую секунду
+setInterval(tick, 1000);
